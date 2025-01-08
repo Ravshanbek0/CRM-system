9c7a39b8b641a5c7b2252719106a342b8406f94a
Ravshanbek0 <rnabijonov66@gmail.com>
2025-01-08 20:08:26 +0500

    In App.jsx get for pupil function added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './page/Home/Home';
 
 
 function App() {
+  const [data, setData] = useState([]); // Ma'lumotni saqlash uchun holat
+  const [loading, setLoading] = useState(true); // Yuklanish holatini ko'rsatish uchun
+  const [error, setError] = useState(null); // Xatolarni saqlash uchun
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://crm-project.up.railway.app/api/v1/pupil/'); // API URL
+      setData(response.data); // Javobni saqlash
+      setLoading(false); // Yuklashni to'xtatish
+    } catch (err) {
+      setError(err.message); // Xatoni saqlash
+      setLoading(false); // Yuklashni to'xtatish
+    }
+  };
+  useEffect(()=>{
+    fetchData()
+    console.log(data);
+    
+  },[])
+
   return (
     <>
       <BrowserRouter>
@@ -14,8 +36,8 @@ function App() {
           <Route path='/payment' element={<Home />} />
           <Route path='/attandance' element={<Home />} />
 
-          <Route path='/students' element={<Home/>}/>
-          <Route path='/groups' element={<Home/>}/>
+          <Route path='/students' element={<Home />} />
+          <Route path='/groups' element={<Home />} />
 
           <Route path='/attendenceGroup' element={<Home />} />
 
